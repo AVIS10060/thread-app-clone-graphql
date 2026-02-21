f7a180fa2c103c044367ac670d3ab96b4c787047
AVIS10060 <avisharmaid@gmail.com>
2026-02-21 14:31:57 +0530

    b

---
+++ src/graphql/index.ts
@@ -0,0 +1,42 @@
+import { prismaClient } from '../lib/db.js';
+import { ApolloServer } from '@apollo/server';
+import { User } from './user/index.js';
+
+
+
+async function createApolloGraphqlServer(){
+
+
+
+    const gqlServer = new ApolloServer({
+  typeDefs: `
+  type Query {
+        hello:String
+    
+  }
+  type Mutation{
+        ${User.mutations}
+    
+    }
+  `,
+  resolvers:{
+    Query:{
+        ...User.resolvers.queries,
+    },
+    Mutation: {
+       ...User.resolvers.mutations,
+
+    }
+
+  },
+});
+
+
+
+await gqlServer.start();
+
+ return gqlServer
+
+}
+
+export default createApolloGraphqlServer
